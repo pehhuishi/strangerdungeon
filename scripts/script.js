e844742efabd38cb76b8c1c7cc404f05875a12ee
Prima Aulia Gusta <primAulia@gmail.com>
2017-11-30 17:23:22 +0800

    latest

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,177 @@
-const API_URL="http://www.dnd5eapi.co/api/classes/",ASSETS_URL="./imgs/classes",BEYOND_URL="https://www.dndbeyond.com/characters/classes/",Home={props:["classes"],template:'\n    <div class="row wrapper">\n      <div v-for="cls in classes"\n           :class="[\'one-half\', \'column\', \'character-column\', cls.name]"\n           @click="goToClass(cls.url)">\n        <span>{{ cls.name }}</span>\n      </div>\n    </div>\n  ',methods:{goToClass:function(e){const a=/classes\/((\d){1,2})/g.exec(e)[1];this.$router.push({path:`/class/${a}`})}}},Detail={data:function(){return{character:{},equipments:[]}},template:'\n    <div class="row wrapper">\n      <div class="character-hero one-half column">\n        <a :href="character.readMoreUrl" target="_blank">\n          <h2>{{ character.name }}</h2>\n        </a>\n        <img :src="character.image" :alt="character.name" width="100%">\n        <a class="button button-primary" href="#" @click="goBack">Back</a>\n      </div>\n      <div class="character-status one-half column">\n        <div class="hp attribute">\n          <h3>Hit Points</h3>\n          <ul>\n            <li>Hit Dice: 1d{{ character.hit_die }} per {{ character.name }} level</li>\n            <li>HP at 1st Level: {{ character.hit_die }} + your Constitution modifier\n            <li>HP at Higher Levels: 1d{{ character.hit_die}} + your Constitution modifier per {{ character.name }} level after 1st</li>\n          </ul>\n        </div>\n        <div class="proficiencies attribute">\n          <h3>Proficiencies</h3>\n          <ul>\n            <li v-for="proficiency in character.proficiences">\n              {{ proficiency.name }}\n            </li>\n          </ul>\n        </div>\n        <div class="saving-throws attribute">\n          <h3>Saves</h3>\n          <ul>\n            <li v-for="saving_throw in character.saving_throws">\n              {{ saving_throw.name }}\n            </li>\n          </ul>\n        </div>\n        <div class="equipments attribute">\n          <h3>Starting Equipment</h3>\n          <p v-if="equipments.length === 0">None</p>\n          <ul>\n            <li v-for="equipment in equipments">\n              {{ equipment.quantity }} {{ equipment.item.name }}\n            </li>\n          </ul>\n        </div>\n        <a class="button button-primary" :href="character.readMoreUrl" target="_blank">Read more</a>\n      </div>\n      <div class="side-navi u-full-width">\n        <a @click="prevClass">\n          <i class="fa fa-4x fa-angle-left"></i>\n        </a>\n        <a @click="nextClass">\n          <i class="fa fa-4x fa-angle-right"></i>\n        </a>\n      </div>\n    </div>\n  ',created(){this.retrieveClass(this.$route.params.id)},watch:{character:function(e){this.retrieveEquipments(e)}},beforeRouteUpdate(e,a,t){this.retrieveClass(e.params.id),t()},methods:{goBack:function(){this.$router.push("/")},retrieveClass:function(e){axios.get(API_URL+e).then(e=>{data=e.data,this.character=data,this.character.image=`${ASSETS_URL}/${data.name}.png`,this.character.proficiences=data.proficiencies,this.character.readMoreUrl=`${BEYOND_URL}${data.name.toLowerCase()}`,document.querySelector("body").className="",document.querySelector("body").classList.add(data.name)}).catch(e=>{console.log(e)})},retrieveEquipments:function(e){axios.get(e.starting_equipment.url).then(a=>{e=a.data,this.equipments=e.starting_equipment}).catch(e=>{console.log(e)})},prevClass:function(){let e="1"===this.$route.params.id?12:parseInt(this.$route.params.id)-1;this.$router.push({path:`/class/${e}`})},nextClass:function(){let e="12"===this.$route.params.id?1:parseInt(this.$route.params.id)+1;this.$router.push({path:`/class/${e}`})}}},routes=[{path:"/",component:Home},{path:"/class/:id",component:Detail}],app=new Vue({router:new VueRouter({routes:routes}),data:{title:"Stranger Dungeon",classes:[]},mounted:function(){this.retrieveClasses()},methods:{retrieveClasses:function(){axios.get(API_URL).then(e=>{this.classes=e.data.results}).catch(e=>{console.error(e)})}}}).$mount("#app");
+// Init Vue!
+const API_URL = 'http://www.dnd5eapi.co/api/classes/'
+const ASSETS_URL = './imgs/classes'
+const BEYOND_URL = 'http://www.dndbeyond.com/characters/classes/'
+
+// Home component
+const Home = {
+  props: ['classes'],
+  template: `
+    <div class="row wrapper">
+      <div v-for="cls in classes"
+           :class="['one-half', 'column', 'character-column', cls.name]"
+           @click="goToClass(cls.url)">
+        <span>{{ cls.name }}</span>
+      </div>
+    </div>
+  `,
+  methods: {
+    goToClass: function (url) {
+      const classId = /classes\/((\d){1,2})/g.exec(url)[1] // derive class id by the class api endpoint
+      this.$router.push({ path: `/class/${classId}` })
+    }
+  }
+}
+
+// Details component
+const Detail = {
+  data: function () {
+    return {
+      character: {},
+      equipments: []
+    }
+  },
+  template: `
+    <div class="row wrapper">
+      <div class="character-hero one-half column">
+        <a :href="character.readMoreUrl" target="_blank">
+          <h2>{{ character.name }}</h2>
+        </a>
+        <img :src="character.image" :alt="character.name" width="100%">
+        <a class="button button-primary" href="#" @click="goBack">Back</a>
+      </div>
+      <div class="character-status one-half column">
+        <div class="hp attribute">
+          <h3>Hit Points</h3>
+          <ul>
+            <li>Hit Dice: 1d{{ character.hit_die }} per {{ character.name }} level</li>
+            <li>HP at 1st Level: {{ character.hit_die }} + your Constitution modifier
+            <li>HP at Higher Levels: 1d{{ character.hit_die}} + your Constitution modifier per {{ character.name }} level after 1st</li>
+          </ul>
+        </div>
+        <div class="proficiencies attribute">
+          <h3>Proficiencies</h3>
+          <ul>
+            <li v-for="proficiency in character.proficiences">
+              {{ proficiency.name }}
+            </li>
+          </ul>
+        </div>
+        <div class="saving-throws attribute">
+          <h3>Saves</h3>
+          <ul>
+            <li v-for="saving_throw in character.saving_throws">
+              {{ saving_throw.name }}
+            </li>
+          </ul>
+        </div>
+        <div class="equipments attribute">
+          <h3>Starting Equipment</h3>
+          <p v-if="equipments.length === 0">None</p>
+          <ul>
+            <li v-for="equipment in equipments">
+              {{ equipment.quantity }} {{ equipment.item.name }}
+            </li>
+          </ul>
+        </div>
+        <a class="button button-primary" :href="character.readMoreUrl" target="_blank">Read more</a>
+      </div>
+      <div class="side-navi u-full-width">
+        <a @click="prevClass">
+          <i class="fa fa-4x fa-angle-left"></i>
+        </a>
+        <a @click="nextClass">
+          <i class="fa fa-4x fa-angle-right"></i>
+        </a>
+      </div>
+    </div>
+  `,
+  created () {
+    this.retrieveClass(this.$route.params.id)
+  },
+  watch: {
+    character: function (data) {
+      this.retrieveEquipments(data)
+    }
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.retrieveClass(to.params.id)
+    next()
+  },
+  methods: {
+    goBack: function () {
+      this.$router.push('/')
+    },
+    retrieveClass: function (classId) {
+      axios.get(API_URL + classId)
+      .then(response => {
+        data = response.data
+
+        this.character = data
+        this.character.image = `${ASSETS_URL}/${data.name}.png`
+        this.character.proficiences = data.proficiencies
+        this.character.readMoreUrl = `${BEYOND_URL}${data.name.toLowerCase()}`
+
+        // change body class per character name
+        document.querySelector('body').className = ''
+        document.querySelector('body').classList.add(data.name)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+    retrieveEquipments: function (data) {
+      axios.get(data.starting_equipment.url)
+           .then(response => {
+             data = response.data
+             this.equipments = data.starting_equipment
+           })
+           .catch(err => {
+             console.log(err)
+           })
+    },
+    prevClass: function () {
+      let prevParam = this.$route.params.id === '1' ? 12 : parseInt(this.$route.params.id) - 1
+      this.$router.push({ path: `/class/${prevParam}` })
+    },
+    nextClass: function () {
+      let nextParam = this.$route.params.id === '12' ? 1 : parseInt(this.$route.params.id) + 1
+      this.$router.push({ path: `/class/${nextParam}` })
+    }
+  }
+}
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/class/:id',
+    component: Detail
+  }
+]
+
+const app = new Vue({
+  router: new VueRouter({
+    routes
+  }),
+  data: {
+    title: 'Stranger Dungeon',
+    classes: []
+  },
+  mounted: function () {
+    this.retrieveClasses()
+  },
+  methods: {
+    retrieveClasses: function () {
+      axios.get(API_URL)
+      .then(response => {
+        this.classes = response.data.results
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    }
+  }
+}).$mount('#app')
